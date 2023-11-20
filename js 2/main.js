b14b1848a3a9301742b646c09ce35b61c473b1fa
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-11-20 02:54:05 -0300

    practica objetos y clases

---
+++ js 2/main.js
@@ -0,0 +1,42 @@
+//objetos
+let coche = {
+    modelo: "Renault",
+    maxima: 300,
+    antiguedad: 2015,
+    mostrarDatos(){
+        console.log(this.modelo, this.maxima, this.antiguedad);
+        document.write(this.modelo);
+        document.write(this.propiedad1);
+    },
+    propiedad1: "valor aleatorio",
+};
+document.write("<h2>" + coche.modelo + "</h2>")
+coche.mostrarDatos();
+
+//clases 
+
+class Coche {
+    constructor(modelo, velocidad, antiguedad) {
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+        this.antiguedad = antiguedad;
+    }
+
+    aumentarVelocidad() {
+        this.velocidad += 1;
+    }
+
+    reducirVelocidad() {
+        this.velocidad -= 1;
+    }
+}
+
+let coche1 = new Coche('BMW', 300, 2017);
+let coche2 = new Coche('Audi', 400, 2000);
+
+document.write("<h1>Velocidad: "+ coche1.velocidad+"</h1>")
+coche1.aumentarVelocidad();
+coche1.aumentarVelocidad();
+coche1.aumentarVelocidad();
+coche1.aumentarVelocidad();
+document.write("<h1>Velocidad: "+ coche1.velocidad+"</h1>")
