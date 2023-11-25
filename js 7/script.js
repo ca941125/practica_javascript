282f84f13fb89826629ca469a2e09d05003134fa
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-11-25 20:56:27 -0300

    practicando objetos

---
+++ js 7/script.js
@@ -0,0 +1,40 @@
+//OBJETOS:
+
+const unArrayDeObjetos = [
+    { nombre: "Marta", edad: 35 },
+    { nombre: "Susanita", edad: 18 },
+    { nombre: "Horacio", edad: 56 },
+    { nombre: "Lucia", edad: 7 },
+  ];
+  
+  const persona = {
+    nombre: "Carolina",
+    apellido: "Aguirre",
+    animalesFavoritos: ["perro", "gato", "jerbito", "CABALLOS"],
+    edad: 28,
+    esEspecial: true,
+    guardoUnObjeto: { algoEnMiObjeto: "una cosa" },
+  };
+  
+  console.log(persona.animalesFavoritos[0]);
+  
+  const productosDeMeLi = [
+    {
+      id: 1,
+      nombre: "camita para gato",
+      precio: 5000,
+      foto: "https://http2.mlstatic.com/D_NQ_NP_2X_856850-MLA54512613734_032023-F.webp",
+    },
+    {
+      id: 2,
+      nombre: "camita para gato bebe",
+      precio: 7000,
+      foto: "https://http2.mlstatic.com/D_NQ_NP_2X_984257-MLA70558295880_072023-F.webp",
+    },
+    {
+      id: 3,
+      nombre: "juguetito",
+      precio: 300,
+      foto: "https://http2.mlstatic.com/D_NQ_NP_2X_666100-MLA52464402275_112022-F.webp",
+    },
+  ];
