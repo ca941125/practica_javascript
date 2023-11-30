ede642e58cb0c4a318b6be9352a3261bdb930746
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-11-30 19:05:33 -0300

    practicando eventos

---
+++ js 8/main.js
@@ -0,0 +1,8 @@
+document.getElementById("boton_color").addEventListener("click", function(){
+    document.body.style.backgroundColor = "#000" ;
+});
+
+document.getElementById("boton_default").addEventListener("click", function(){
+    document.body.style.backgroundColor = "#fff" ;
+});
+
