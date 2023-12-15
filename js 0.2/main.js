cd91c22c52d93d0b5d54e944a03d21e7ac8c6e53
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-12-14 21:11:37 -0300

    param, forEach y Map

--- js 0.2/main.js
+++ js 0.2/main.js
@@ -40,3 +40,42 @@ console.log(aprendiendoA());
 console.log(aprendiendoB());
 console.log(aprendiendoC());
 console.log(aprendiendoD());
+
+//* Parametros en un arrow Function...
+
+
+// Parametros
+const aprendiendoE = (tecnologia) => console.log(`aprendiendo ${tecnologia}`);
+
+aprendiendoE('JavaScript');
+
+// si es un solo parmetro no ocupamos el parentesis
+const aprendiendoF = tecnologia => console.log(`aprendiendo ${tecnologia}`);
+
+aprendiendoF('JavaScript');
+
+// multiples parametros si requieren parentesis
+const aprendiendoG = (tecn1, tecn2) => console.log(`Aprendiendo ${tecn1} ${tecn2}`);
+
+aprendiendoE('JS', 'ES');
+
+
+
+//* ForEach y Map con arrow functions...
+
+const carrito2 = [
+    { nombre: 'Monitor 20 Pulgadas', precio: 500},
+    { nombre: 'Televisión 50 Pulgadas', precio: 700},
+    { nombre: 'Tablet ', precio: 300},
+    { nombre: 'Audifonos', precio: 200},
+    { nombre: 'Teclado', precio: 50},
+    { nombre: 'Celular', precio: 500},
+]
+
+
+const nuevoArray3 = carrito2.map(  producto =>  `Articulo: ${ producto.nombre } Precio: $ producto.precio} `)
+
+const nuevoArray4 = carrito2.forEach( producto =>  console.log( `Articulo: ${ producto.nombre } Precio: $ producto.precio} `));
+
+console.log(nuevoArray3);
+console.log(nuevoArray4);
