61bb43914905592a305f5473d417c061ec38ef6a
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-12-24 00:00:51 -0300

    agregue funciones class dos

--- js 8/main.js
+++ js 8/main.js
@@ -6,3 +6,11 @@ document.getElementById("boton_default").addEventListener("click", function(){
     document.body.style.backgroundColor = "#fff" ;
 });
 
+
+document.getElementById("boton_uno").addEventListener("click", function(){
+    document.getElementById("boton_uno").style.backgroundColor = "#000";
+})
+
+document.getElementById("boton_dos").addEventListener("click", function(){
+    document.getElementById("boton_uno").style.backgroundColor = "brown";
+})
